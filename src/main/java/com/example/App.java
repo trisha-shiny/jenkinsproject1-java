b93ebb1b7d0c02e24bbfcb3cd6fb1ead38395379
Trisha Shiny <trishashiny2@gmail.com>
2026-04-08 12:49:48 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! Webhook ");
+        System.out.println("Hello, Jenkins! Webhook changes");
     }
 
     public int add(int a, int b) {
